45029a572e668419d8622cfe2670906f2aabd9ac
Yevhenii Kravchenko <yevhenii.kravchenko@gmail.com>
2017-09-03 23:50:15 +0300

    Finish UI for Upload page

--- src/containers/UploadContainer.vue
+++ src/containers/UploadContainer.vue
@@ -1,15 +1,39 @@
 <template>
-  <md-layout md-align="center">
+  <md-layout md-column md-align="center" md-vertical-align="center">
     <h2>Upload</h2>
+    <md-layout md-column class="container" md-vertical-align="center">
+      <md-input-container>
+        <label>Date</label>
+        <md-input type="date" v-model="date"></md-input>
+      </md-input-container>
+      <md-input-container>
+        <md-file v-model="file" placeholder="Select file with next menu" accept="csv"></md-file>
+      </md-input-container>
+      <md-button class="md-raised md-accent upload-button">Upload menu</md-button>
+    </md-layout>
   </md-layout>
 </template>
 
 <script>
   export default {
     name: 'upload-container',
+    data() {
+      return {
+        date: new Date().toISOString().substr(0, 10),
+        file: '',
+      };
+    },
   };
 </script>
 
 <style scoped>
+  .container {
+    width: 90%;
+    max-width: 600px;
+  }
 
+  .upload-button {
+    width: 240px;
+    height: 60px;
+  }
 </style>
